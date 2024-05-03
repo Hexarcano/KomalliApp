2a84b6639f70dbbf52c31412cee2fb06e284da6c
Bjulianms3 <bernardojulianmarinsanchez0@gmail.com>
2024-05-03 13:11:45 -0600

    Agregar pantallas

---
+++ app/src/main/java/mx/uv/komalliapp/ActivityLogin.kt
@@ -0,0 +1,42 @@
+package mx.uv.komalliapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        val bt_inicio = findViewById<Button>(R.id.bt_sesion)
+        val texUsuario = findViewById<EditText>(R.id.tx_usuario)
+        val texPass = findViewById<EditText>(R.id.tx_password)
+
+        bt_inicio.setOnClickListener{
+            val usuario = texUsuario.text.toString()
+            val pass = texPass.text.toString()
+
+            if(usuario == "juli"){
+                if(pass == "juli"){
+                    val intent = Intent(this, ActivityMenu::class.java)
+                    intent.putExtra("usuario",usuario)
+                    startActivity(intent)
+                }
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
