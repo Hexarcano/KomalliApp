e392d22e80ee2bdbf4d41ad29c216a2a10761575
Bjulianms3 <bernardojulianmarinsanchez0@gmail.com>
2024-06-13 21:00:29 -0600

    boton regresar en ayuda

--- app/src/main/java/mx/uv/komalliapp/ActivityAyuda.kt
+++ app/src/main/java/mx/uv/komalliapp/ActivityAyuda.kt
@@ -1,6 +1,8 @@
 package mx.uv.komalliapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +10,9 @@ import androidx.core.view.WindowInsetsCompat
 
 class ActivityAyuda : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val bt_regresar = findViewById<ImageView>(R.id.btn_regresar);
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_ayuda)
@@ -15,6 +20,11 @@ class ActivityAyuda : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        bt_regresar.setOnClickListener{
+            val intent = Intent(this,ActivityMenu::class.java)
+            startActivity(intent);
         }
     }
 }
